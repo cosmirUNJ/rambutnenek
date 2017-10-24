fdda52074c75d4f2ad20e093bb1eece7d3633821
riswandy <riswandy53@gmail.com>
2017-10-24 11:05:04 +0700

    animation work

--- core/src/com/cosmirunj/eelbat/PlayGameStage.java
+++ core/src/com/cosmirunj/eelbat/PlayGameStage.java
@@ -1,5 +1,7 @@
 package com.cosmirunj.eelbat;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -42,8 +44,83 @@ class PlayGameStage extends Stage {
 
         characterEelBat = new CharacterEelBat(eelbatCosmir);
         characterEelBat.updatePosition(x,y,DIRECTION.NONE);
+        addActor(characterEelBat);
 
         //backgroundTiles.update(x,y);
+
+    }
+
+    @Override
+    public void act(float delta) {
+        updateLocation(delta);
+        super.act(delta);
+    }
+
+    private void updateLocation(float delta) {
+        DIRECTION direction;
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) && Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            direction = DIRECTION.RIGHT_UP;
+        } else if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) && Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            direction = DIRECTION.RIGHT_DOWN;
+        } else if(Gdx.input.isKeyPressed(Input.Keys.LEFT) && Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            direction = DIRECTION.LEFT_UP;
+        } else if(Gdx.input.isKeyPressed(Input.Keys.LEFT) && Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            direction = DIRECTION.LEFT_DOWN;
+        } else if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            direction = DIRECTION.RIGHT;
+        } else if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            direction = DIRECTION.LEFT;
+        } else if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            direction = DIRECTION.UP;
+        } else if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            direction = DIRECTION.DOWN;
+        } else {
+            direction = DIRECTION.NONE;
+        }
+
+        if(Gdx.input.isKeyJustPressed(Input.Keys.W)) {
+            sendMainWave();
+        }
+
+        float sq = (float)Math.sqrt(2);
+        if(direction == DIRECTION.RIGHT_UP || direction == DIRECTION.RIGHT_DOWN) {
+            //x += delta*GGJ2017.RUN_SPEED/sq;
+            //x ++;
+            x += delta*800/sq;
+        } else if(direction == DIRECTION.RIGHT) {
+            x += delta*800;
+            //x ++;
+        } else if(direction == DIRECTION.LEFT_UP || direction == DIRECTION.LEFT_DOWN) {
+            //x -= delta*GGJ2017.RUN_SPEED/sq;
+            //x --;
+            x -= delta*800/sq;
+        } else if(direction == DIRECTION.LEFT) {
+            //x -= delta*GGJ2017.RUN_SPEED;
+            //x --;
+            x -= delta*800;
+        }
+
+        if(direction == DIRECTION.RIGHT_UP || direction == DIRECTION.LEFT_UP) {
+            //y += delta*GGJ2017.RUN_SPEED/sq;
+            //y ++;
+            y += delta*800/sq;
+        } else if(direction == DIRECTION.UP) {
+            //y += delta*GGJ2017.RUN_SPEED;
+            //y ++;
+            y += delta*800;
+        } else if(direction == DIRECTION.RIGHT_DOWN || direction == DIRECTION.LEFT_DOWN) {
+            //y -= delta*GGJ2017.RUN_SPEED/sq;
+            //y --;
+            y -= delta*800/sq;
+        } else if(direction == DIRECTION.DOWN) {
+            //y -= delta*GGJ2017.RUN_SPEED;
+            //y --;
+            y -= delta*800;
+        }
+        cameraPosition.x = x;
+        cameraPosition.y = y;
+
+        characterEelBat.updatePosition(x, y, direction);
 
     }
 
