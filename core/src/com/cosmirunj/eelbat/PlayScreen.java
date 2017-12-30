e5558a91c193e8637e16633bcec7dcc2b4d6939f
Gwoks <gwoksz@gmail.com>
2017-12-30 23:10:38 +0700

    pause fix
    alhamdulillah bisa

--- core/src/com/cosmirunj/eelbat/PlayScreen.java
+++ core/src/com/cosmirunj/eelbat/PlayScreen.java
@@ -76,7 +76,6 @@ class PlayScreen implements Screen {
 
     private Stage ButtonStage;
     boolean btnPause;
-    private MODE_GAME mode_game;
     public int enemySpeed;
     int aksesorisSpeed;
     //boolean btnResume;
@@ -105,7 +104,6 @@ class PlayScreen implements Screen {
         isPaused = false;
 
         ButtonStage=new Stage();
-        mode_game = MODE_GAME.MULAI;
 
         btnPause = true;
 
@@ -184,42 +182,38 @@ class PlayScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        //tambahan
-        //batch.begin();
-        switch (mode_game){
-            case MULAI:
+        if(!isPaused) {
+            //tambahan
+            //batch.begin();
+            gameStage.act(delta);
 
-                break;
-            case PAUSEE:
 
-                break;
-            default:
-                break;
+
+            playHUD.act(delta);
+
+
+
+            //tambahan juga
+            //batch.end();
+            stage.act(delta);
+
+            ButtonStage.act(Gdx.graphics.getDeltaTime());
+
+            playHUD.update(delta);
         }
-
-        gameStage.act(delta);
         gameViewport.apply();
         gameStage.draw();
 
-
-        playHUD.act(delta);
         hudViewport.apply();
         playHUD.draw();
 
-
-        //tambahan juga
-        //batch.end();
-        stage.act(delta);
         stage.draw();
 
-        ButtonStage.act(Gdx.graphics.getDeltaTime());
+
         ButtonStage.draw();
-
-        playHUD.update(delta);
-
-        if(!isPaused){
-            Gdx.graphics.requestRendering();
-        }
+//        if(!isPaused){
+//            Gdx.graphics.requestRendering();
+//        }
     }
 
     public void setSpeedPause() {
@@ -325,8 +319,8 @@ class PlayScreen implements Screen {
                 ButtonSetting.setVisible(!btnPause);
                 ButtonExit.setVisible(!btnPause);
                 //pauseLabel.setVisible(true);
-                mode_game = MODE_GAME.MULAI;
 
+                isPaused = false;
                 return btnPause;
             }
 
@@ -430,10 +424,11 @@ class PlayScreen implements Screen {
                 ButtonSetting.setVisible(!btnPause);
                 ButtonExit.setVisible(!btnPause);
                 //pauseLabel.setVisible(true);
-                mode_game = MODE_GAME.PAUSEE;
                 Gdx.graphics.setContinuousRendering(true);
                 //Gdx.graphics.requestRendering();
                 //Gdx.app.getApplicationListener().dispose();
+
+                isPaused = true;
                 return btnPause;
             }
 
@@ -513,6 +508,4 @@ class PlayScreen implements Screen {
         }
         sonarButtonActive = true;
     }
-
-    public enum MODE_GAME{MULAI, PAUSEE}
 }
